4fc8944c9f9eb3e21b401b384008dd7de187d867
Leon-azerty <maxime.noel@epitech.eu>
2025-03-11 21:23:17 +0100

    fix: replace input by shadcn  input

--- src/app/(auth)/components/Search.tsx
+++ src/app/(auth)/components/Search.tsx
@@ -1,3 +1,4 @@
+import { Input } from '@/ui/input'
 import { SearchIcon } from 'lucide-react'
 
 interface ISearchProps {
@@ -9,13 +10,13 @@ export default function Search(props: ISearchProps) {
   return (
     <div className="relative w-full max-w-md">
       <SearchIcon
-        className="absolute left-3 top-1/2 -translate-y-1/2 transform text-gray-400"
+        className="absolute left-3 top-1/2 -translate-y-1/2 transform"
         size={18}
       />
-      <input
+      <Input
         type="text"
         placeholder="Search recipe with title or ingredients..."
-        className="w-full rounded-lg border border-gray-300 py-2 pl-10 pr-4 shadow-sm outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
+        className="w-full rounded-lg border py-2 pl-10 pr-4 shadow-sm outline-none"
         value={props.search}
         onChange={(e) => props.setSearch(e.target.value)}
       />
