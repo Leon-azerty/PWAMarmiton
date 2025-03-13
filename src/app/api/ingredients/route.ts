32462a29616299e89b845de04a56d9bd818cfd4e
Leon-azerty <maxime.noel@epitech.eu>
2025-03-13 07:46:05 +0100

    refactor: remove unused code

--- src/app/api/ingredients/route.ts
+++ src/app/api/ingredients/route.ts
@@ -1,12 +1,5 @@
 import prisma from '@/lib/prisma'
-import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
-
-// Initialisation du client Supabase
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
-)
 
 export async function GET() {
   try {
